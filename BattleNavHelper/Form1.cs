19b5770eb7a42eb37e6734ec244ec1ed9225fe4f
Kevin Laemers <18590570+PloxKevin@users.noreply.github.com>
2020-06-28 16:56:36 +0200

    Attempting to fix the bnav calc

--- BattleNavHelper/Form1.cs
+++ BattleNavHelper/Form1.cs
@@ -5,6 +5,8 @@ namespace BattleNavHelper
 {
     public partial class Form1 : Form
     {
+
+        
         public static double hit_me = 0;
         public static double shot_me = 0;
         public static double our_max_goal = 6;
@@ -15,15 +17,24 @@ namespace BattleNavHelper
         public Form1()
         {
             InitializeComponent();
+
         }
 
+        private void setFields()
+        {
+            TheyHitUsAmount.Text = Convert.ToString(hit_them);
+            TheyShotTotal.Text = Convert.ToString(shot_them);
+            TheirMaxGoal.Text = Convert.ToString(their_max_goal);
+            WeHitThemAmount.Text = Convert.ToString(hit_me);
+            WeShotTotal.Text = Convert.ToString(shot_me);
+            OurMaxGoal.Text = Convert.ToString(our_max_goal);
+            PercentageHit.Text = Convert.ToString(Math.Round((hit_me * 100 / shot_me),2));
+        }
         private void TheyHitUs(object sender, EventArgs e)
         {
             hit_them += 1;
             shot_them += 1;
-            TheyHitUsAmount.Text = Convert.ToString(hit_them);
-            TheyShotTotal.Text = Convert.ToString(shot_them);
-            TheirMaxGoal.Text = Convert.ToString(their_max_goal);
+            setFields();
         }
 
 
@@ -31,78 +42,82 @@ namespace BattleNavHelper
         {
             hit_me += 1;
             shot_me += 1;
-            WeHitThemAmount.Text = Convert.ToString(hit_me);
-            WeShotTotal.Text = Convert.ToString(shot_me);
-            OurMaxGoal.Text = Convert.ToString(our_max_goal);
-            PercentageHit.Text = Convert.ToString(Math.Round((hit_me * 100 / shot_me),2));
+            setFields();
         }
 
         private void WeMissedThem(object sender, EventArgs e)
         {
             shot_me += 1;
-            WeHitThemAmount.Text = Convert.ToString(hit_me);
-            WeShotTotal.Text = Convert.ToString(shot_me);
-            OurMaxGoal.Text = Convert.ToString(our_max_goal);
-            PercentageHit.Text = Convert.ToString(Math.Round((hit_me * 100 / shot_me),2));
+            setFields();
         }
 
         private void TheyMissedUs(object sender, EventArgs e)
         {
             shot_them += 1;
-            TheyHitUsAmount.Text = Convert.ToString(hit_them);
-            TheyShotTotal.Text = Convert.ToString(shot_them);
-            TheirMaxGoal.Text = Convert.ToString(their_max_goal);
+            setFields();
         }
-
-        private void ShipThem_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            foreach (var ship in Ship.Values)
-            {
-                if (ShipThem.SelectedItem.ToString().Equals(ship.Name))
-                {
-                    var our_cannon_size = ship.Cannon_Size;
-                    if (our_cannon_size == "Small")
-                    {
-                        our_max_goal = ship.Max_Small;
-                    }
-                    else if (our_cannon_size == "Medium")
-                    {
-                        our_max_goal = ship.Max_Medium;
-                    }
-                    else if (our_cannon_size == "Large")
-                    {
-                        our_max_goal = ship.Max_Large;
-                    }
-                }
-            }
-        }
-
 
         private void ShipUs_SelectedIndexChanged(object sender, EventArgs e)
         {
-            foreach (var ship in Ship.Values)
+            foreach (var shipthem in Ship.Values)
             {
-                if (ShipUs.SelectedItem.ToString().Equals(ship.Name))
+                if (ShipThem.SelectedItem.ToString().Equals(shipthem.Name))
                 {
-                    var their_cannon_size = ship.Cannon_Size;
-                    if (their_cannon_size == "Small")
+                    foreach (var shipus in Ship.Values)
                     {
-                        their_max_goal = ship.Max_Small;
-                    }
-                    else if (their_cannon_size == "Medium")
-                    {
-                        their_max_goal = ship.Max_Medium;
-                    }
-                    else if (their_cannon_size == "Large")
-                    {
-                        their_max_goal = ship.Max_Large;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Failure");
+                        if (ShipUs.SelectedItem.ToString().Equals(shipus.Name))
+                        {
+                            var our_cannon_size = shipus.Cannon_Size;
+                            if (our_cannon_size == "Small")
+                            {
+                                our_max_goal = shipthem.Max_Small;
+                            }
+                            else if (our_cannon_size == "Medium")
+                            {
+                                our_max_goal = shipthem.Max_Medium;
+                            }
+                            else if (our_cannon_size == "Large")
+                            {
+                                our_max_goal = shipthem.Max_Large;
+                            }
+                        }
                     }
                 }
+                
             }
+            
+        }
+
+
+        private void ShipThem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (var shipus in Ship.Values)
+            {
+                if (ShipUs.SelectedItem.ToString().Equals(shipus.Name))
+                {
+                    foreach (var shipthem in Ship.Values)
+                    {
+                        if (ShipUs.SelectedItem.ToString().Equals(shipthem.Name))
+                        {
+                            var their_cannon_size = shipthem.Cannon_Size;
+                            if (their_cannon_size == "Small")
+                            {
+                                their_max_goal = shipus.Max_Small;
+                            }
+                            else if (their_cannon_size == "Medium")
+                            {
+                                their_max_goal = shipthem.Max_Medium;
+                            }
+                            else if (their_cannon_size == "Large")
+                            {
+                                their_max_goal = shipthem.Max_Large;
+                            }
+                        }
+                    }
+                }
+                
+            }
+
         }
 
         private void ResetButton(object sender, EventArgs e)
